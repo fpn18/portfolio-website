681e23f040c7eae3334df22a89c166e4f540cd98
fpn18 <felicia.ng23@imperial.ac.uk>
2025-09-07 16:15:45 +0100

    changes

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-// Remove smooth scroll for nav links to allow navigation to other HTML files
 
-// Optional: Add other interactive JS later (animations, project filters, etc.)
-    e.preventDefault();
-    const target = document.querySelector(this.getAttribute('href'));
-    target.scrollIntoView({ behavior: 'smooth' });
-  });
-});
-
-// Optional: Add other interactive JS later (animations, project filters, etc.)
